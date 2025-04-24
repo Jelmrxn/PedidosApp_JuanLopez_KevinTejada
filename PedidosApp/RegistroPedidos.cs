64f78010f800f17c782d25c52cc1107d088f34a7
Jelmrxn <jelmrxn@gmail.com>
2025-04-24 10:22:35 -0500

    Clase RegistroPedidos patron singleton

---
+++ PedidosApp/RegistroPedidos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+
+    //Clase RegistroPedidos patron singleton
+    public sealed class RegistroPedidos
+    {
+        private static RegistroPedidos _instancia;
+        private static readonly object _lock = new object();
+
+        public List<Pedido> Pedidos { get; private set; }
+
+        private RegistroPedidos() => Pedidos = new List<Pedido>();
+
+        public static RegistroPedidos Instancia
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instancia ??= new RegistroPedidos();
+                }
+            }
+        }
+
+        public void AgregarPedido(Pedido pedido)
+        {
+            Pedidos.Add(pedido);
+        }
+    }
+}
