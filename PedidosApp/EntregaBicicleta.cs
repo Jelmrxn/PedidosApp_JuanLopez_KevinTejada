6a642992308c6fecc1e9a6a9e4e313803c53a110
Jelmrxn <jelmrxn@gmail.com>
2025-04-29 18:20:04 -0500

    Modificacion para implementar a la logica el nuevo metodo de entrega EntregaBicicleta()

---
+++ PedidosApp/EntregaBicicleta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public class EntregaBicicleta : IMetodoEntrega
+    {
+        public double CalcularCosto(int km)
+        {
+            return 3 * km;
+        }
+
+        public string TipoEntrega()
+        {
+            return "Entrega en Bicicleta";
+        }
+    }
+}
--- PedidosApp/EntregaFactory.cs
+++ PedidosApp/EntregaFactory.cs
@@ -10,21 +10,22 @@ namespace PedidosApp
     //Implementacion patron factory clase EntregaFactory
     public static class EntregaFactory
     {
-        public static class EntregaFactory
+
+        //Modificacion para implementar a la logica el nuevo metodo de entrega EntregaBicicleta()
+        public static IMetodoEntrega CrearEntrega(string tipoProducto, bool urgente, double peso)
         {
-            public static IMetodoEntrega CrearEntrega(string tipoProducto, bool urgente, double peso)
-            {
-                if (peso > 10)
-                    return new EntregaCamion();
-                else if (tipoProducto == "tecnología" && urgente)
-                    return new EntregaDron();
-                else if (tipoProducto == "accesorio")
-                    return new EntregaMoto();
-                else if (tipoProducto == "componente")
-                    return new EntregaCamion();
-                else
-                    return new EntregaMoto(); // Por defecto
-            }
+            if (tipoProducto == "accesorio" && peso < 2 && !urgente)
+                return new EntregaBicicleta();
+            else if (peso > 10)
+                return new EntregaCamion();
+            else if (tipoProducto == "tecnología" && urgente)
+                return new EntregaDron();
+            else if (tipoProducto == "accesorio")
+                return new EntregaMoto();
+            else if (tipoProducto == "componente")
+                return new EntregaCamion();
+            else
+                return new EntregaMoto(); // Por defecto
         }
     }
 }
